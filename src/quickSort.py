693271baedbdddc8d799808a0b821fbb007bbe3d
asantiagoq <82891107+asantiagoq@users.noreply.github.com>
2021-12-19 20:55:10 -0500

    Agregando el codigo de quicksort en python

---
+++ src/quickSort.py
@@ -0,0 +1,23 @@
+def division(arr, l, h):
+    i = (l-1)         
+    pivot = arr[h]     
+ 
+    for j in range(l, h):
+ 
+        if arr[j] <= pivot:
+ 
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+ 
+ 
+def quickSort(arr, l, h):
+    if len(arr) == 1:
+        return arr
+    if l < h:
+ 
+        pi = division(arr, l, h)
+        quickSort(arr, l, pi-1)
+        quickSort(arr, pi+1, h)
